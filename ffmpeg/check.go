c0e6ba02da1fc4886f811e821c1c21b9d912802b
Defly Wang <defly@yeah.net>
2026-03-05 21:32:22 +0800

    优化FFmpeg查找：优先从exe同目录查找，支持便携式部署

--- ffmpeg/check.go
+++ ffmpeg/check.go
@@ -2,15 +2,48 @@ package ffmpeg
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
 )
 
+func FindFFmpeg() string {
+	ext := ""
+	if os.PathSeparator == '\\' {
+		ext = ".exe"
+	}
+
+	execDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	localFFmpeg := filepath.Join(execDir, "ffmpeg"+ext)
+	if _, err := os.Stat(localFFmpeg); err == nil {
+		return localFFmpeg
+	}
+
+	path, err := exec.LookPath("ffmpeg" + ext)
+	if err == nil {
+		return path
+	}
+
+	return ""
+}
+
 func CheckFFmpeg() error {
-	cmd := exec.Command("ffmpeg", "-version")
+	ffmpegPath := FindFFmpeg()
+	if ffmpegPath == "" {
+		return fmt.Errorf("ffmpeg not found. Please install ffmpeg")
+	}
+
+	cmd := exec.Command(ffmpegPath, "-version")
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("ffmpeg not found. Please install ffmpeg")
 	}
-	cmd = exec.Command("ffprobe", "-version")
+
+	ffprobePath := filepath.Join(filepath.Dir(ffmpegPath), "ffprobe.exe")
+	if _, err := os.Stat(ffprobePath); err != nil {
+		return fmt.Errorf("ffprobe not found. Please install ffprobe")
+	}
+
+	cmd = exec.Command(ffprobePath, "-version")
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("ffprobe not found. Please install ffprobe")
 	}
